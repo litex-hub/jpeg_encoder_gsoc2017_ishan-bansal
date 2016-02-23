f266b3fbb0d3d7946eb6de5ea3b982e16b24dcf7
Florent Kermarrec <florent@enjoy-digital.fr>
2016-02-23 16:51:38 +0100

    test/model/encoder: add zigzag

--- test/model/encoder.py
+++ test/model/encoder.py
@@ -82,10 +82,26 @@ def dct(block):
 	return dct_block
 
 def quantize(block, table):
-    r  = [0 for i in range(64)]
+    r  = [0]*64
     for i, (value, divider) in enumerate(zip(block, table)):
         r[i] = int(value/divider)
     return r
+
+zig_zag_order = [
+ 0,  1,  5,  6, 14, 15, 27, 28,
+ 2,  4,  7, 13, 16, 26, 29, 42,
+ 3,  8, 12, 17, 25, 30, 41, 43,
+ 9, 11, 18, 24, 31, 40, 44, 53,
+10, 19, 23, 32, 39, 45, 52, 54,
+20, 22, 33, 38, 46, 51, 55, 60,
+21, 34, 37, 47, 50, 56, 59, 61,
+35, 36, 48, 49, 57, 58, 62, 63]
+
+def zigzag(block):
+	r = [0]*64
+	for i in range(64):
+		r[zig_zag_order[i]] = block[i]
+	return r
 
 from reference import *
 
@@ -104,7 +120,7 @@ print(cr444_16x8_blocks[0])
 print("dct")
 print("="*40)
 print(dct(y422_16x8_blocks[0]))
-print(y422_dct_16x8_block[0])
+print(y422_dct_16x8_blocks[0])
 print(dct(cb422_16x8_block))
 print(cb422_dct_16x8_block)
 
@@ -114,3 +130,8 @@ print(quantize(cb422_dct_16x8_block, cbcr_quant_table))
 print(cb422_qwant_16x8_block)
 print(quantize(cr422_dct_16x8_block, cbcr_quant_table))
 print(cr422_qwant_16x8_block)
+
+print("zigzag")
+print("="*40)
+print(zigzag(y422_qwant_16x8_blocks[0]))
+print(y422_zigzag_16x8_blocks[0])
